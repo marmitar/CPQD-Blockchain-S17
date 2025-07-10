b2a5aa03ff5e20c6354d771e16fc1a3a21dae42a
Tiago de Paula <tiagodepalves@gmail.com>
2025-07-10 20:53:51 -0300

    feat: challenge 1 setup

---
+++ test/Challenge1.t.sol
@@ -0,0 +1,18 @@
+// SPDX-License-Identifier: AGPL-3.0-or-later
+pragma solidity >=0.8.13 <0.9.0;
+
+import { Test } from "forge-std/Test.sol";
+
+import { TrabalhoERC20 } from "../src/Challenge1.sol";
+
+contract Challenge1Test is Test {
+    TrabalhoERC20 public erc20 = TrabalhoERC20(address(0));
+
+    function setUp() public {
+        erc20 = new TrabalhoERC20();
+    }
+
+    function test_Initialized() public view {
+        assertNotEq(address(erc20), address(0));
+    }
+}
