a119f9ab94ee9d8ee93d46cdf5b7c58d18cde5c0
Tiago de Paula <tiagodepalves@gmail.com>
2025-07-28 18:38:14 -0300

    gist: delay LUT creation

--- src/gist/Sqrt.sol
+++ src/gist/Sqrt.sol
@@ -1,6 +1,6 @@
 // SPDX-License-Identifier: GPL-2.0-only
 // Gas efficient SQRT method, computes floor(sqrt(x)).
-// Constant gas cost of 439.
+// Constant gas cost of 351.
 //
 // Authors: Lohann Ferreira, Tiago de Paula
 pragma solidity >=0.7.0 <0.9.0;
@@ -8,32 +8,15 @@ pragma solidity >=0.7.0 <0.9.0;
 function sqrt(uint256 x) pure returns (uint256 r) {
     assembly ("memory-safe") {
         // r = floor(log2(x))
-        r := shl(7, gt(x, 0xFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFF))
-        let xx := shr(r, x)
+        r := shl(7, lt(0xFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFF, x))
+        r := or(r, shl(6, lt(0xFFFFFFFFFFFFFFFF, shr(r, x))))
+        r := or(r, shl(5, lt(0xFFFFFFFF, shr(r, x))))
+        r := or(r, shl(4, lt(0xFFFF, shr(r, x))))
+        r := or(r, shl(3, lt(0xFF, shr(r, x))))
+        r := or(r, shl(2, lt(0xF, shr(r, x))))
 
-        let rr := shl(6, gt(x, 0xFFFFFFFFFFFFFFFF))
-        xx := shr(rr, xx)
-        r := or(r, rr)
-
-        rr := shl(5, gt(xx, 0xFFFFFFFF))
-        xx := shr(rr, xx)
-        r := or(r, rr)
-
-        rr := shl(4, gt(xx, 0xFFFF))
-        xx := shr(rr, xx)
-        r := or(r, rr)
-
-        rr := shl(3, gt(xx, 0xFF))
-        xx := shr(rr, xx)
-        r := or(r, rr)
-
-        rr := shl(2, gt(xx, 0x0F))
-        xx := shr(rr, xx)
-        r := or(r, rr)
-
-        rr := shl(1, gt(xx, 0x03))
-        xx := shr(rr, xx)
-        r := or(r, rr)
+        let lut := 0x0000010102020202030303030303030300000000000000000000000000000000
+        r := or(r, byte(shr(r, x), lut))
 
         r := shl(shr(1, r), 1)
 
