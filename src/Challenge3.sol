f0c2e05ec0a44a2742f4feb96204a221df8547b7
Tiago de Paula <tiagodepalves@gmail.com>
2025-07-28 02:22:49 -0300

    feat(extra): approximated de bruijn sequence

--- src/Challenge3.sol
+++ src/Challenge3.sol
@@ -2,44 +2,52 @@
 pragma solidity ^0.8.27;
 
 /**
- * @notice Calculate the integer square root of `x`.
- * @dev Based on <https://github.com/PaulRBerg/prb-math/blob/v4.1.0/src/Common.sol#L587-L675>.
+ * @notice Efficient Integer Square Root method.
+ * @dev Based on <https://gist.github.com/Lohann/f01e2558691ba8648f1e5a7c6e8d37da>.
  */
 function sqrt(uint256 x) pure returns (uint256 root) {
     assembly ("memory-safe") {
-        // Find the most significant bit, equivalent to `log2(x)`.
-        let xAux := x
-        // msb7 = (x >= 2**128) ? 128 : 0
-        let msb7 := shl(7, gt(xAux, 0xFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFF))
-        xAux := shr(msb7, xAux) // x >> msb7
-        // msb6 = (x >= 2**64) ? 64 : 0
-        let msb6 := shl(6, gt(xAux, 0xFFFFFFFFFFFFFFFF))
-        xAux := shr(msb6, xAux) // x >> msb6
-        // msb5 = (x >= 2**32) ? 32 : 0
-        let msb5 := shl(5, gt(xAux, 0xFFFFFFFF))
-        xAux := shr(msb5, xAux) // x >> msb5
-        // msb4 = (x >= 2**16) ? 16 : 0
-        let msb4 := shl(4, gt(xAux, 0xFFFF))
-        xAux := shr(msb4, xAux) // x >> msb4
-        // msb3 = (x >= 2**8) ? 8 : 0
-        let msb3 := shl(3, gt(xAux, 0xFF))
-        xAux := shr(msb3, xAux) // x >> msb3
-        // msb2 = (x >= 2**4) ? 4 : 0
-        let msb2 := shl(2, gt(xAux, 0xF))
-        xAux := shr(msb2, xAux) // x >> msb2
-        // msb1 = (x >= 2**2) ? 2 : 0
-        let msb1 := shl(1, gt(xAux, 0x3))
-        // SKIPPED: x >> msb1
-        // SKIPPED: msb0 = (x >= 2**1) ? 1 : 0
-        // msb = msb1 | msb2 | msb3 | msb4 | msb5 | msb6 | msb7
-        let msb := or(or(or(or(or(or(msb1, msb2), msb3), msb4), msb5), msb6), msb7)
+        // First, we find an approximation for $\log_2(x)$.
+        //
+        // The following algorithm does a "binary seach" over the regions of $x$, updating our approximation of
+        // $\log_2(x)$ depending on wether the current value is bigger or lower than threshold (i.e. the highest bit is
+        // left or right of a specific region). At each iteration, it gives one extra bit of information on the result
+        // of $\log_2(x)$.
+        let log := shl(7, lt(0xFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFF, x))
+        log := or(log, shl(6, lt(0xffffffffffffffff, shr(log, x))))
+        log := or(log, shl(5, lt(0xffffffff, shr(log, x))))
+        log := or(log, shl(4, lt(0xffff, shr(log, x))))
+        log := or(log, shl(3, lt(0xff, shr(log, x))))
 
-        // Get the closest power of two: $x_0 = 2^{\lfloor \log_2(x) / 2 \rfloor}$.
-        // This will be our initial guess for Newton's method, which has at least one bit correct.
-        root := shl(shr(1, msb), 1)
-        // Now we have 1 bit correct of $\sqrt{x}$. The next 7 iterations of Newton's method will expand it into 256
-        // bits.
-        // Note: division by zero returns 0, so not an issue here.
+        // At this point, our $\log_2(x)$ has the 5 upper bits right. For the remaining bits, uses can a lookup table
+        // based on a De Bruijn sequence to improve our approximation.
+        //
+        // For 3 bits in log space we would need 8 bits of input, so a 256 byte table. We can actually skip the last
+        // bit, because we'll just use $log_2(x) / 2$, not the full $\log_2(x)$. The last bit of $\log_2(x)$ represents
+        //  2 bits in $x$, so our table is reduced to the following 64 bytes:
+        // 0x00020202040404040404040404040404060606060606060606060606060606060606060606060606060606060606060606060606060606060606060606060606
+        //
+        // This still doesn't fit in 32 bytes, but we can abuse the redundancy in the table to reach the last values.
+        // Specifically, 0x06 appears on all bytes from position 16 to 63, which means that it is accessed whenever the
+        // index matches 0x170. In this case, an index $i$ smaller than 31 can be used directly, but an index of 32-63
+        // more can be reduced to $i >> 1$, which maps to 16-31 and gives the same result 0x06. This can be expressed
+        // as $i >> (i > 31)$ or equivalently as $i >> (i >> 5)$.
+        let table := 0x02020204040404040404040404040406060606060606060606060606060606
+
+        let i := shr(2, shr(log, x))
+        i := shr(shr(5, i), i)
+        log := or(log, byte(i, table))
+
+        // Here we have an value almost equal to $\log_2(x)$, except maybe for the last bit. This is not an issue,
+        // because we'll use $2^{\log_2(x)/2} \approx \sqrt{x}$ as an initial approximation for the square root.
+        // This value ensures that:
+        // \[ 2^{\log_2(x)/2} \leq \sqrt{x} < 2^{\log_2(x)/2 + 1} \]
+        // So at least the first bit is correct.
+        root := shl(shr(1, log), 1)
+
+        // Now we have 1 bit correct of $\sqrt{x}$, and we need 128 bits for the roots of all `uint256` numbers. Each
+        // iteration of the Newton's method doubles the precision, so 7 iterations are required.
+        // Note: division by zero returns 0, giving the correct result for $\sqrt{0}$.
         root := shr(1, add(root, div(x, root)))
         root := shr(1, add(root, div(x, root)))
         root := shr(1, add(root, div(x, root)))
@@ -47,7 +55,7 @@ function sqrt(uint256 x) pure returns (uint256 root) {
         root := shr(1, add(root, div(x, root)))
         root := shr(1, add(root, div(x, root)))
         root := shr(1, add(root, div(x, root)))
-        // For almost perfect squares, Newton's method cycles between `⌊√x⌋` => `⌊√x⌋ + 1`, so we need to
+        // For almost perfect squares, Newton's method cycles between ⌊√x⌋ and ⌊√x⌋ + 1, so we need to
         // round the result down.
         root := sub(root, gt(root, div(x, root)))
     }
