899873ae9919869f2de0cba62229cfbd16dac919
Tiago de Paula <tiagodepalves@gmail.com>
2025-07-29 00:42:33 -0300

    chore: bump EVM to Prague

--- src/Challenge2.sol
+++ src/Challenge2.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: AGPL-3.0-or-later
-pragma solidity ^0.8.27;
+pragma solidity ^0.8.30;
 
 /**
  * @notice Calculate the integer approximation for area the of a circle of radius `r`.
